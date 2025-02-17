2ec15e3f454d68f4376f42562a70bc80176fe921
Bryan Alexander Alvarado Medina <bryanalex2017@gmail.com>
2025-02-17 00:02:02 -0600

    gh-pages added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/portafolio-bryan",
+  trailingSlash: true,
 };
 
 export default nextConfig;
